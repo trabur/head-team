e83f59fa8e16dac1f05c9504eaba60b70ac958a2
trabur <travis.burandt@gmal.com>
2020-05-07 16:18:39 -0500

    tick <--> tock

--- gear.js
+++ gear.js
@@ -9,6 +9,7 @@ function noop() { return null }
 let teeth = [
   // {
   //   id: UUID(),
+  //   gearId: UUID(), // cassette
   //   type: 'interval' || 'timeout',
   //   function: noop(),
   //   duration: 100, // ms
@@ -80,7 +81,7 @@ export async function rotate(/* amount */) {
     // rule: interval gears are executed first
     let intervalGears = filterByType(teeth, 'interval')
     // rule: check if it should run during this spin cycle
-    let durationIntervalGears = filterByDuration(intervalGears, now, offset, tick, tock) 
+    let durationIntervalGears = filterByDuration(intervalGears, offset, spin, turn, tick, tock) 
     // rule: local functions take priority
     let spacedIntervalGears = sortByDistance(durationIntervalGears) 
 
@@ -99,7 +100,7 @@ export async function rotate(/* amount */) {
     // rule: timeout gears are executed second
     let timeoutGears = filterByType(teeth, 'timeout')
     // rule: check if it should run during this spin cycle
-    let durationTimeoutGears = filterByDuration(timeoutGears, now, offset, tick, tock) 
+    let durationTimeoutGears = filterByDuration(timeoutGears, offset, spin, turn, tick, tock) 
     // rule: local functions take priority
     let spacedTimeoutGears = sortByDistance(durationTimeoutGears) 
 
@@ -140,8 +141,21 @@ function filterByType(teeth, type) {
   })
 }
 
-function filterByDuration(teeth, now, offset, tick, tock) {
+function filterByDuration(teeth, offset, spin, turn, tick, tock) {
+  // first: 10       // ms from start
+  // now: 11         // ms this instant
+  // spin: 1         // small rotation
+  // turn: .1        // large rotation
+  // tick: 10        // spin sleep cycle in ms
+  // tock: 100       // turn sleep cycle in ms
 
+  // offset is how long it's been sense the last time around
+  // tooth.duration is how long we want this tooth to sleep for
+  // we want to find every tooth that fits within this time frame
+  // between: now and the next time around
+  return teeth.filter((tooth) => {
+    return tick <= (offset + tooth.duration) <= tock // if true then tooth will run
+  })
 }
 
 function smallestDuration(teeth) {
@@ -202,143 +216,3 @@ function update(gear) {
   let index = findIndexById(gear.id)
   teeth[index] = gear
 }
-
-/**
- * ========
- * // everything under this block of code is expiramental
- * ========
- */
-// function calculate (gear) {
-//   // this method inserts a new gear into the gears array by offsetting
-//   // it's placement so that when it hits the top of the stack 
-//   // it is able to execute on time.
-
-//   // each gear has a turnAt ... stopAt so the timing can be figured out
-//   // for timeout gears: we loop over the stack of gears adding up their
-//   // run times. when we reach the end of the stack or the point when
-//   // our timeout gear needs to execute --> then we insert.
-
-//   let now = new Date().getTime()
-
-//   // is the stack of gears is empty?
-//   if (gears.length === 0) {
-//     // add our new gear to it anyways
-//     gears.push(gear)
-//     return gear
-//   }
-
-//   // what type of gear is this?
-//   if (gear.type === 'interval') {
-//     // put gear right on top of the stack
-//     gears.unshift(gear)
-
-//   } else {
-//     // this is a timeout gear:
-//     // counter keeps track of offset on our stack of gears in ms 
-//     // as we loop though it
-//     let firstCounter = 0
-//     let counter = 0
-//     let nextCounter = 0
-//     let insertAt = gears.length - 1 // by default insert new gear at the end
-//     gears.forEach((value, index) => {
-//       // figure out when is this gear going to run, for how long, then tally up counter
-//       let distance = getDistance(value)
-
-//       // increase run time offset
-//       firstCounter = counter
-//       counter = counter + distance
-
-//       // will this gear be in the proper order if inserted here?
-//       if (gear.duration <= counter) {
-//         // we need this gear to run under the counter so when it executes it is on time
-//         // is this index better than the previous insertAt?
-//         if (true) {
-//           // yes
-//           insertAt = index 
-//         }
-//       }
-//     })
-    
-//     // at position insertAt, remove 0 elements, then add gear to that position
-//     gears.splice(insertAt, 0, gear)
-//     return gear
-//   }
-// }
-
-// function getDistance(gear) {
-//   let distance = 0
-//   if (gear.type === 'interval') {
-//     // it's going to run emediately; how long did it take to execute last time?
-//     distance = gear.stopAt - gear.turnAt // 12 - 10 = 2
-//   } else {
-//     // this is a timeout gear:
-//     // it's going to run later; how long did it take to execute last time?
-//     distance = gear.stopAt - gear.turnAt + gear.duration // 12 - 10 + 1 = 3
-//   }
-//   return distance
-// }
-
-// async function offset(gear) {
-//   let now = new Date().getTime()
-//   let when = now - gear.turnAt + gear.duration // 11 - 10 + 1 = 2
-//   if (when <= 0) {
-//     // it needs to run now // false
-//     return execute(gear)
-//   } else {
-//     // it needs to run later // true
-//     await sleep(when)
-//     return execute(gear)
-//   }
-// }
-
-// export default function run() {
-//   // every time we insert a new gear into the system we make sure
-//   // it is in the proper order by run time.
-
-//   // calling this method will loop through our gears array.
-//   // after grabbing the first gear off the top of the stack run
-//   // will then decide if it needs to execute it's function right
-//   // now or create a setTimeout and then execute it later.
-  
-//   // if turnAt is set to null that means look at the type
-//   // timeout: we wait for the given duration then run emidiately and finish
-//   // interval: we run emidiately and then again after the given duration
-
-//   // rule: place interval gears before timeout gears
-
-//   // when run decides to shift a gear it will log turnAt,
-//   // then execute, and then log stopAt.
-
-//   // after a gear has been shifted off the top of the stack run
-//   // will then loop back around to the --> next --> gear ... 
-//   // calculate // execute and repeat
-
-//   // start with one gear at a time.
-//   let first = gears[0]
-//   // have we been here before?
-//   if (first.turn === null) {
-//     // this gear has never been turned before
-//     if (first.type === 'interval') {
-//       // execute it emediately
-//       let gear = execute(first)
-//       // add our new gear to the bottom
-//       gears.push(gear)
-//       // remove first gear from the top
-//       gears = gears.slice(1)
-//       // again
-//       return gear
-//     } else {
-//       // this is a timeout gear:
-//       if (first.turnAt === null) {
-//         // it needs to run now if not sooner
-//         return execute(first)
-//       } else {
-//         // it might need to run later or maybe now
-//         return offset(first)
-//       }
-//     }
-//   } else {
-//     // this gear has been turned before
-//     return offset(first)
-//   }
-// }
