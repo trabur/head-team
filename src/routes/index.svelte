4e6f2166a290b1737a2d12a7b5d70835a610ad27
trabur <travis.burandt@gmal.com>
2020-12-21 11:59:09 -0600

    cleanup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,19 +26,4 @@
 	import Welcome from '$components/Welcome';
 	import Navigation from '$components/Navigation';
 
-	// onMount(() => {
-  //   // let gun = new Gun(['https://gunjs.herokuapp.com/gun']);
-	// 	// let user = gun.user();
-	// 	// user.recall({ sessionStorage: true }, (user) => {
-	// 	// 	console.log('hello,', user.put.alias)
-	// 	// 	console.log(user)
-	// 	// 	username = user.put.alias
-	// 	// 	pub = user.put.pub
-	// 	// })
-
-  //   // google analytics
-  //   gtag('config', 'UA-161516824-1', {
-  //     'page_path': window.location.pathname
-  //   });
-	// })
 </script>
